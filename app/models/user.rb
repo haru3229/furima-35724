7b872883faefa44cb5aeae7355bb3b42b2dbadc2
haru3229 <kotaro.f28@gmail.com>
2021-05-28 12:36:36 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :items
+  has_many :orders
+
+  validates :nickname,    presence: true
+  validates :family_name, presence: true
+  validates :first_name,  presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
 end
