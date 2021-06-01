469f6fae1d8f28487a576c40a066dd9acc1320bf
haru3229 <kotaro.f28@gmail.com>
2021-06-01 17:40:54 +0900

    バリデーションの設定（英数字，全角等の細かい設定）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,14 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all
   end
 
+  def show
+  end
+
   def new
     @item = Item.new
-  end
-
-  def create
-    Item.create(item_params)
-  end
-
-  private
-  def item_params
-    params.require(:item).permit(:nickname, :image)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end      
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :items
   has_many :orders
 
-  validates :nickname,    presence: true
-  validates :family_name, presence: true
-  validates :first_name,  presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
+
+  with_options presence: true do
+   validates :nickname       
+   validates :birth_day
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  #英字と数字の両方が必要
+  validates_format_of :password,with: PASSWORD_REGEX
+
+  
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/} do
+  #全角ひらがな，カタカナ，漢字のみ
+    validates :family_name
+    validates :first_name
+  end
+  
+  with_options format: { with: /\A[ァ-ヶー－]+\z/} do
+  #全角カタカナのみ
+    validates :family_name_kana
+    validates :first_name_kana
+    end
 end
