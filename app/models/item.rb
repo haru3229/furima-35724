94cc3b99e181af8a5330e469912b1667a39802da
haru3229 <kotaro.f28@gmail.com>
2021-06-06 10:00:44 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :order
+  has_one_attached :image
 
   with_options presence: true do
    validates :name
