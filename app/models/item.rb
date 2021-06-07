b7daf0b2f8a39b70be3e9b741f236474c612b9f4
haru3229 <kotaro.f28@gmail.com>
2021-06-07 13:49:37 +0900

    商品一覧表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,11 +17,11 @@ class Item < ApplicationRecord
     validates :price, format: { with: /\A[0-9]+\z/ },
                       numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     with_options numericality: { other_than: 1 } do
-      validates :category_id                          
-      validates :status_id                         
-      validates :cost_id                         
-      validates :brand_id                           
-      validates :day_id                            
+      validates :category_id
+      validates :status_id
+      validates :cost_id
+      validates :brand_id
+      validates :day_id
     end
   end
 end
